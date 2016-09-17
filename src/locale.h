8dc97fab8095ac0e138a93717695d89bcce20318
koekeishiya <aasvi93@hotmail.com>
2016-09-17 18:13:18 +0200

    forward declare struct instead of including header

--- src/locale.h
+++ src/locale.h
@@ -1,8 +1,7 @@
 #ifndef KHD_LOCALE_H
 #define KHD_LOCALE_H
 
-#include "hotkey.h"
-
+struct hotkey;
 bool KeycodeFromChar(char Key, hotkey *Hotkey);
 bool LayoutIndependentKeycode(char *Key, hotkey *Hotkey);
 bool StringsAreEqual(const char *A, const char *B);
